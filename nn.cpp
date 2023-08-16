1ea91317edee995ccd3c4e40b3af07af482a3c2e
kombofuud <piershanson@gmail.com>
2023-08-16 01:31:06 +0000

    added in extra column to account for the bias

--- nn.cpp
+++ nn.cpp
@@ -23,8 +23,7 @@ int main(){
 std::vector<std::vector<std::vector<std::vector<float>>>>  makeResNet(std::vector<int> layerSizes){ //Creates a neural network with the specified layer sizes at the end of each residual block.
     std::vector<std::vector<std::vector<std::vector<float>>>> resNet; //output vector containing a list of the residual blocks
     for(int i = 0; i < layerSizes.size()-1; i++){
-        //add a residual block for each specification
-        resNet.push_back(makeResBlock(layerSizes[i], layerSizes[i+1]));
+        resNet.push_back(makeResBlock(layerSizes[i], layerSizes[i+1])); //add a residual block for each specification
     }
     return resNet;
 }
@@ -40,42 +39,43 @@ std::vector<std::vector<std::vector<float>>> makeResBlock(int input, int output)
 
 
 std::vector<std::vector<float>> makeMat(int height, int width){ //Makes a matrix initialized using a He distribution
+    height++; // add an extra column for the bias
     const float heConst = sqrt(2.0/height);
     std::vector<std::vector<float>> columns;
     for(int i = 0; i < height; i++){
         std::vector<float> row;
         for(int j = 0; j < width; j++){
-            row.push_back(heConst*normalD(ranlux48));
+            row.push_back(heConst*normalD(ranlux48)); //makes the elements of each row
         }
-        columns.push_back(row);
+        columns.push_back(row); //inserts each column into the matrix
     }
     return columns;
 }
 
 
-void visualizeNet(std::vector<std::vector<std::vector<std::vector<float>>>> net){
-    for(int i = 0; i < net.size(); i++){
-        for(int j = 0; j < net[i][0].size(); j++){
+void visualizeNet(std::vector<std::vector<std::vector<std::vector<float>>>> net){ //displays the matrices that make up the given each neural network
+    for(int i = 0; i < net.size(); i++){ //loops though each res block
+        for(int j = 0; j < net[i][0].size(); j++){ //first nn in block
             for(int k = 0; k < net[i][0][0].size(); k++){
-                std::cout << net[i][0][j][k] << " ";
+                std::cout << net[i][0][j][k] << " "; //separates each element of a row with a space
             }
-            std::cout << std::endl;
+            std::cout << std::endl; //puts the next column on the next line
         }
-        std:: cout << std::endl;
+        std:: cout << std::endl; // adds an additional line between the matrices
         for(int j = 0; j < net[i][1].size(); j++){
-            for(int k = 0; k < net[i][1][0].size(); k++){
+            for(int k = 0; k < net[i][1][0].size(); k++){ // second nn in block
                 std::cout << net[i][1][j][k] << " ";
             }
             std::cout << std::endl;
         }
         std:: cout << std::endl;
         for(int j = 0; j < net[i][2].size(); j++){
-            for(int k = 0; k < net[i][2][0].size(); k++){
+            for(int k = 0; k < net[i][2][0].size(); k++){ // input linearizer
                 std::cout << net[i][2][j][k] << " ";
             }
             std::cout << std::endl;
         }
-        std:: cout << std::endl << std::endl;
+        std:: cout << std::endl << std::endl; //adds a second empty line between residual blocks
     }
 }
 std::vector<float> activation(std::vector<float> input){ //Takes a list of numbers and applies an activation function to them before spitting them back out
